bc6ac349a8e4ff3eb049479ba8003fa7f9ec796d
domco545 <43946320+domco545@users.noreply.github.com>
2021-03-24 21:37:40 +0100

    fixed import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { StockModule } from './stock/api/stock.module';
 
 @Module({
   imports: [StockModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
